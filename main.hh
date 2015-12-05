dd65630f43b2abd33205e5b1b1dd4deb2a387a43
steelbrain <aneesiqbalbhatti@gmail.com>
2015-12-05 13:31:40 -0700

    :new: Implement MySQL methods The real macros

--- main.hh
+++ main.hh
@@ -2,6 +2,7 @@
 
 namespace steelbrain;
 use PDO;
+use PDOStatement;
 
 type MySQLconfig = shape(
   'User' => string,
@@ -13,6 +14,50 @@ type MySQLconfig = shape(
 class MySQL {
   public int $lastInsertedId = 0;
   public function __construct(public PDO $link) {}
+  public function query(string $statement, ?array<string, mixed> $parameters = null): PDOStatement {
+    $query = $this->link->prepare($statement);
+    if ($parameters !== null) {
+      $query->execute($parameters);
+    }
+    return $query;
+  }
+  public function fetch<T>(string $statement, ?array<string, mixed> $parameters = null): ?T {
+    $result = $this->query($statement, $parameters)->fetch(PDO::FETCH_ASSOC);
+    if ($result === false) {
+      return null;
+    } else return $result;
+  }
+  public function fetchCol<T>(string $statement, ?array<string, mixed> $parameters = null): ?T {
+    $result = $this->query($statement, $parameters)->fetch(PDO::FETCH_NUM);
+    if ($result === false) {
+      return null;
+    } else return $result[0];
+  }
+  public function fetchAll<T>(string $statement, ?array<string, mixed> $parameters = null): array<T> {
+    return $this->query($statement, $parameters)->fetchAll(PDO::FETCH_ASSOC);
+  }
+  public function exists(string $statement, ?array<string, mixed> $parameters = null): bool {
+    return $this->query($statement, $parameters)->rowCount() > 0;
+  }
+  public function insert(string $table, array<string, mixed> $paramters): int {
+    $keys = [];
+    $values = [];
+    $placeholders = [];
+
+    foreach($paramters as $key => $value) {
+      $keys[] = $key;
+      $values[":$key"] = $value;
+      $placeholders[] = ":$key";
+    }
+
+    $keys = implode(', ', $keys);
+    $placeholders = implode(', ', $placeholders);
+
+    $query = $this->query("Insert into $table ($keys) VALUES ($placeholders)", $values);
+
+    return $this->lastInsertedId = $this->link->lastInsertId();
+  }
+
   public static function create(MySQLconfig $config, string $charset = 'utf8mb4'): MySQL {
     $link = new PDO("mysql:charset=$charset;host=$config[Host];dbname=$config[Name]", $config['User'], $config['Pass'], [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
